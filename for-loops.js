123143dbee55277b2b5ad9a2f4eb971689915d46
Lauren Kight <kight6@gmail.com>
2016-07-10 11:53:30 -0500

    completed basic for loops

--- for-loops.js
+++ for-loops.js
@@ -10,15 +10,10 @@ for (var i = 5; i <= 120; i += 10) {
 // Use console.log() to output its value every time. When the counter variable's value is 1, 
 // stop execution.
 // 
-for (var i = 4096; i >= 1; i /= 2)
+for (var i = 4096; i >= 1; i /= 2) {
   console.log(i)
 }
 
 // Create an array that contains the names of American Presidents. 
 // Loop over that array with a for loop, and use string concatenation with console.log() 
 // to output the order and name of each President (see example below).
-var presidents = ['Abraham Lincoln', 'George Washington', 'Thomas Jefferson', 'Barack Obama', 'Bill Clinton', 'George Bush']
-
-for (var i = 0; i <= presidents.length; i++) {
-  console.log()
-}
