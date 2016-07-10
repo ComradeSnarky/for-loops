cd2c00aa3d8a3004a0fb914ab8fe5bd290ac56f7
Lauren Kight <kight6@gmail.com>
2016-07-10 10:32:20 -0500

    testing for loops

---
+++ for-loops.js
@@ -0,0 +1,6 @@
+// Write a for loop that uses a counter variable initialized at 5 and then increments it by 10 every time it executes. Use console.log() 
+// to output the value of the counter variable each time through the loop. Stop execution of the loop if the counter variable's value is greater than 120.
+
+for (var i = 0; i <= 10; i++) {
+  console.log(i);
+}
